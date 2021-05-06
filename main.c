18106f331494550d2c7389ea574a400841580c2d
ciro_falsarella <ciro.falsarella@serhcm.com>
2021-05-06 17:40:03 -0300

    trabalho final

---
+++ main.c
@@ -0,0 +1,11 @@
+/****************************************
+ *  11795593 - Ciro Grossi Falsarella   *
+ *  11795641 - Pedro Martelleto         *
+ ****************************************/
+
+#include <stdio.h>
+
+
+int main(){
+    return 0;
+}
